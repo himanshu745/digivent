c384827f15662d25bb7d4c86da2180e33461efe0
himanshu745 <himanshurajpal33@gmail>
2020-09-17 11:13:45 +1200

    user login

--- digivent-backend/routes/users.js
+++ digivent-backend/routes/users.js
@@ -12,4 +12,30 @@ router.get("/", (req, res, next) => {
     .catch(next);
 });
 
+router.post("/", (req, res, next) => {
+ console.log(req.body)
+ res.send(req.body); 
+});
+
+// endpoint for logging in
+router.post("/login", (req, res, next) => {
+  
+  if (!req.body.email) {
+    return res.status(422).send("Email can't be blank");
+  }
+
+
+  User.findOne({ email: req.body.email })
+    .then(function (user) {
+      if (!user) {
+        
+        return res.status(422).send("User not found");
+      }
+      return res.send(user);
+    })
+    .catch(next);
+});
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+
 module.exports = router;
