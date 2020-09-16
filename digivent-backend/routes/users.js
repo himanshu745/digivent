a42f155dd704dee3e4e3069b1437d5575f46e562
himanshu745 <himanshurajpal33@gmail>
2020-09-17 11:58:20 +1200

    user login

--- digivent-backend/routes/users.js
+++ digivent-backend/routes/users.js
@@ -3,19 +3,32 @@ const router = require("express").Router();
 const User = require("../models/User.js");
 const Event = require("../models/Event.js");
 
-router.get("/", (req, res, next) => {
-    Event.find({}) 
-    .sort({ createdAt: "desc" })
-    .then((results) => {
-      return res.send(results + "Users");
+router.param("id", (req, res, next, id) => {
+  User.findById(id)
+    .then((user) => {
+      if (!user) {
+        res.status(404).send("User not found");
+      } else {
+        req.user = user;
+        return next();
+      }
     })
     .catch(next);
 });
 
-router.post("/", (req, res, next) => {
- console.log(req.body)
- res.send(req.body); 
+router.get("/", (req, res, next) => {
+  User.find({})
+    .sort({ createdAt: "desc" })
+    .then((users) => {
+      return res.status(200).send(users);
+    })
+    .catch(next);
 });
+
+router.get("/:id", (req, res, next) => {
+  res.status(200).send(req.user);
+});
+
 
 // endpoint for logging in
 router.post("/login", (req, res, next) => {
@@ -33,6 +46,63 @@ router.post("/login", (req, res, next) => {
       }
       return res.send(user);
     })
+    .catch(next); 
+});
+
+router.post("/register", function (req, res, next) {
+  //server-side validation
+ 
+  
+  if (!req.body.username) {
+    return res.status(422).send("Username can't be blank");
+  }
+  if (!req.body.password) {
+    return res.status(422).send("Username can't be blank");
+  }
+
+  if (!req.body.email) {
+    return res.status(422).send("Email can't be blank");
+  }
+
+  // find a mathcing user based on username
+  User.findOne({ username: req.body.username })
+    .then((user) => {
+      if (user) {
+       
+        return res.status(422).send("User already exists");
+      }
+   
+      const newUser = new User(req.body);
+      newUser
+        .save()
+        
+        .then((result) => {
+          return res.send(result);
+        })
+       
+        .catch(next);
+    })
+
+  
+  // find a mathcing user based on email
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (user) {
+       
+        return res.status(422).send("User already exists");
+      }
+   
+      const newUser = new User(req.body);
+      newUser
+        .save()
+        
+        .then((result) => {
+          return res.send(result);
+        })
+       
+        .catch(next);
+    })
+    
     .catch(next);
 });
 
